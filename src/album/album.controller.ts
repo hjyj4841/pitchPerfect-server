827aadbc45f1da5dbfcb139d3eda4d9e95d8b7a6
Dong Yeop-Lee <33149488+hjyj4841@users.noreply.github.com>
2025-02-10 20:29:54 +0900

    feat: 평점 수정/삭제 && 한줄평 추가/수정/삭제 로직 추가

--- src/album/album.controller.ts
+++ src/album/album.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Request } from '@nestjs/common';
+import { Controller, Post, Body, Request, Put, Get, Param, Req, Delete } from '@nestjs/common';
 import { AlbumService } from './album.service';
 import { Album } from '@prisma/client';
 import { AuthService } from 'src/auth/auth.service';
@@ -10,12 +10,77 @@ export class AlbumController {
     private readonly authService: AuthService
   ) {}
 
-  // 앨범 평가 추가
+  // 앨범 평가 전체 조회
+  @Get()
+  async findAll(): Promise<Album[]>{
+    return this.albumService.findAll();
+  }
+
+  // 앨범 아이디로 조회
+  @Get(':id')
+  async findOnw(@Param('id') albumId: number): Promise<Album | null>{
+    return this.albumService.findOne(albumId);
+  }
+
+  // 앨범 별점 평가 추가
   @Post('scope')
   async addAlbumScope(@Body() album: Album, @Request() req: Request): Promise<Album>{
     const user = this.authService.validateAccessToken(req);
     album.userId = user.userId;
 
     return this.albumService.addAlbumScope(album);
+  }
+
+  // 앨범 별점 수정
+  @Put('scope/:id')
+  async modifyAlbumScope(@Param('id') albumId: number, @Body() body: { albumRate: number }, @Req() req: Request): Promise<Album> {
+    const user = this.authService.validateAccessToken(req);
+    const album: Album | null = await this.albumService.findOne(albumId);
+
+    if(album === null || album.userId !== user.userId){
+      throw new Error;
+    }
+
+    return this.albumService.modifyAlbumScope(albumId, body.albumRate);
+  }
+
+  // 앨범 별점 삭제
+  @Delete('scope/:id')
+  async deleteAlbumScope(@Param('id') albumId: number, @Req() req: Request): Promise<Album>{
+    const user = this.authService.validateAccessToken(req);
+    const album: Album | null = await this.albumService.findOne(albumId);
+
+    if(album === null || album.userId !== user.userId){
+      throw new Error;
+    }
+
+    return this.albumService.deleteAlbumScope(albumId);
+  }
+
+  // 앨범 한줄평 추가 / 수정(한줄평은 앨범 별점 평가 후 가능)
+  @Put('review/:id')
+  async saveAlbumReview(@Param('id') albumId: number, @Body() body: {albumReview: string}, @Req() req: Request): Promise<Album>{
+    const user = this.authService.validateAccessToken(req);
+    const album: Album | null = await this.albumService.findOne(albumId);
+
+    if(album === null || album.userId !== user.userId){
+      throw new Error;
+    }
+    const albumReview: string | null = body.albumReview === '' ? null : body.albumReview;
+
+    return this.albumService.saveAlbumReview(albumId, albumReview);
+  }
+
+  // 앨범 한줄평 삭제
+  @Delete('review/:id')
+  async deleteAlbumReview(@Param('id') albumId: number, @Req() req: Request): Promise<Album>{
+    const user = this.authService.validateAccessToken(req);
+    const album: Album | null = await this.albumService.findOne(albumId);
+
+    if(album === null || album.userId !== user.userId){
+      throw new Error;
+    }
+
+    return this.albumService.deleteAlbumReview(albumId);
   }
 }
--- src/album/album.service.ts
+++ src/album/album.service.ts
@@ -6,6 +6,20 @@ import { PrismaService } from 'src/prisma.service';
 export class AlbumService {
   constructor (private readonly prismaService: PrismaService){}
 
+  // 앨범 평가 전체 조회
+  async findAll(): Promise<Album[]>{
+    return this.prismaService.album.findMany();
+  }
+
+  // 앨범 아이디로 조회
+  async findOne(albumId: number): Promise<Album | null>{
+    return this.prismaService.album.findUnique({
+      where: {
+        albumId: Number(albumId)
+      }
+    });
+  }
+
   // 앨범 별점 추가
   async addAlbumScope(album: Album): Promise<Album>{
     return this.prismaService.album.create({
@@ -13,4 +27,44 @@ export class AlbumService {
     });
   }
   
+  // 앨범 별점 수정
+  async modifyAlbumScope(albumId: number, albumRate: number): Promise<Album> {
+    return this.prismaService.album.update({
+      where: {
+        albumId: Number(albumId)
+      },
+      data: {
+        albumRate: albumRate
+      }
+    });
+  }
+
+  // 앨범 별점 삭제
+  async deleteAlbumScope(albumId: number): Promise<Album>{
+    return this.prismaService.album.delete({
+      where: {albumId: Number(albumId)}
+    });
+  }
+
+  // 앨범 한줄평 추가 및 수정
+  async saveAlbumReview(albumId: number, albumReview: string | null): Promise<Album> {
+    return this.prismaService.album.update({
+      where: {
+        albumId: Number(albumId)
+      },
+      data: {
+        albumReview: albumReview
+      }
+    });
+  }
+
+  // 앨범 한줄평 삭제
+  async deleteAlbumReview(albumId: number): Promise<Album>{
+    return this.prismaService.album.update({
+      where: {albumId: Number(albumId)},
+      data: {
+        albumReview: null
+      }
+    });
+  }
 }
