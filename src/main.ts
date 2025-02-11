8f4d4af9bc0c03e9ddee04e85cf75b6df7a52994
Dong Yeop-Lee <33149488+hjyj4841@users.noreply.github.com>
2025-02-12 00:10:59 +0900

    fix: cors 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,15 @@ import { AppModule } from './app.module';
 dotenv.config();
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeader: 'Content-Type, Authorization',
+  });
+
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
